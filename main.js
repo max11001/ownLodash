2e29d546b6f54c21520df4ec78544b63fff77bbf
Max <verovenkomax@gmail.com>
2022-08-21 14:11:52 +0300

    conact

--- main.js
+++ main.js
@@ -13,6 +13,44 @@ function chunk(arr, chunk){
 }
 
 function compact(arr) {
-    arr = arr.filter(value => value != false);
-    return arr;
+    let newArr = [...arr];
+    newArr = newArr.filter(value => value != false);
+    return newArr;
 }
+/* new */
+function concat(arr, ...values){
+    let newArr = [...arr];
+    values.forEach(elem => { newArr.push(elem); });
+    return newArr;
+}
+
+function drop(firstArr, num) {
+    newArr = [...firstArr];
+    for(let i = 0; i < num; i++){
+        newArr.shift(); 
+    }
+    return newArr;
+}
+
+function dropRight(firstArr, num) {
+    newArr = [...firstArr];
+    for(let i = 0; i < num; i++){
+        newArr.pop(); 
+    }
+    return newArr;
+}
+
+function dropRightWhile(arr, predicate){
+    let newArr = [...arr];
+    for(let i = 0; i < newArr.length; i++){
+        predicate(newArr[i]) ? null : newArr.splice(i,1);
+    }
+    return newArr;
+}
+
+var users = [
+    { 'user': 'barney',  'active': true },
+    { 'user': 'fred',    'active': false },
+    { 'user': 'pebbles', 'active': false }
+  ];
+ console.log(dropRightWhile(users, function(o) { return !o.active; }));  
